85f03ecd0b14c6dacf66d83df75fee4c3ed56fba
Kui <kui_eugenio@hotmail.com>
2017-12-12 10:18:15 -0700

    added some Navbar framework

--- PortHub/client/src/components/LandingPage/LandingPage.js
+++ PortHub/client/src/components/LandingPage/LandingPage.js
@@ -1,10 +1,13 @@
 import React from "react";
 import {Row,Col} from "../Grid";
 import Button from '../../components/Button';
-import Jumbtron from '../Jumbotron'
+import Jumbtron from '../Jumbotron';
+import Navbar from '../Navbar';
+import   './LandingPage.css';
 
 const LandingPage = ({ children }) =>
 <div>
+  <Navbar />
   <Jumbtron>
     <img src='/assets/images/porthub_logo.png' alt="logo" />       
     <p> Create a portfolio website and a resume in minutes!<br/>Stay a step ahead of your competitors in the job market and get that job your deserve.</p>
@@ -12,8 +15,8 @@ const LandingPage = ({ children }) =>
   <div className='position-absolute w-100' style={{bottom:'25%'}}>
     <Row>
       <Col size='lg-12' className='position-absolute'>
-        <Button className="btn btn-danger m-4 p-2">Create a Portfolio Website</Button>
-        <Button className="btn btn-danger  m-4 p-2">Create a Resume</Button>
+        <Button className="btn btn-warning m-4 p-2">Create a Portfolio Website</Button>
+        <Button className="btn btn-info  m-4 p-2">Create a Resume</Button>
       </Col>
     </Row>;
   </div>
---
+++ PortHub/client/src/components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+const Navbar = props =>
+  <nav className="navbar navbar-default text-white">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <div className="navbar-brand" to="/" style={{display:'none'}}>
+          Porthub // make invisible on landing page then visible when other menus
+        </div>
+      </div>
+      <ul className="nav navbar-nav">
+        <li>
+          <i class="fa fa-user-circle" aria-hidden="true"></i>
+          Login
+        </li>
+      </ul>
+    </div>
+  </nav>;
+
+export default Navbar;
