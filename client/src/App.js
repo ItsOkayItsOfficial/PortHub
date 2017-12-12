1f7def2c3cdf4edd24fcbd4af59090a82cd3ff1a
wcrozier12 <wcc8@nau.edu>
2017-12-11 18:34:13 -0700

    removed react logo

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src='' className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to PortHub</h1>
         </header>
         <p className="App-intro">
