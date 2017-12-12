967fa5bf74e1ae7fde679ebd88504a6a1b81f6ca
wcrozier12 <wcc8@nau.edu>
2017-12-11 18:30:06 -0700

    Working structure

--- project3/src/components/Form/FormBtn.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export const FormBtn = props =>
-  <button {...props} style={{ float: "right" }} className="btn btn-success">
-    {props.children}
-  </button>;
--- project3/src/components/Jumbotron/Jumbotron.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Jumbotron = ({ children }) =>
-  <div style={{ height: 300 }} className="jumbotron">
-    {children}
-  </div>;
-
-export default Jumbotron;
