8d42be2d4b56f36c83e2f10e875063edc2dfb4da
wcrozier12 <wcc8@nau.edu>
2017-12-12 15:32:44 -0700

    added template component

---
+++ PortHub/client/src/pages/TemplatePage/Template/Template.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Template = (props) => {
+  return (  <div className="card" style={{width:'20rem'}}>
+      <img className="card-img-top" src="..." alt="Card image cap"></img>
+      <div className="card-block">
+        <h4 className="card-title">Card title</h4>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  )
+}
+
+
+
+export default Template;
--- PortHub/client/src/pages/TemplateSite/TemplateSite.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import   './TemplatePage.css';
-
-const TemplateSite = ({ type, children }) =>
-<div>
-  <Templates type="site" />
-</div>
-export default TemplatePage;
