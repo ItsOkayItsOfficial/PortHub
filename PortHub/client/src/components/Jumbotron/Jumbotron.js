e5b1a952f16a1abf731046f1f9b274a83a842479
Kui <kui_eugenio@hotmail.com>
2017-12-15 11:25:20 -0700

    changed so jumbotron just accepts children

--- PortHub/client/src/components/Jumbotron/Jumbotron.js
+++ PortHub/client/src/components/Jumbotron/Jumbotron.js
@@ -5,8 +5,11 @@ const Jumbotron = ({ children }) =>
   // <div className="jumbotron jumbotron-fluid bg-dark text-white d-flex flex-column align-items-center">
   //   {children}
   // </div>;
-
-  <div className="bg-dark" style={{opacity:'.2', width:'100%', height:'400px'}}>
-    {children}
+  <div>
+      <div className="bg-dark" style={{opacity:'.2', width:'100%', height:'400px'}}>
+      </div>
+      <div className='jumbotron-contents'>
+        {children}
+      </div>
   </div>
 export default Jumbotron;
--- PortHub/client/src/pages/LandingPage/LandingPage.js
+++ PortHub/client/src/pages/LandingPage/LandingPage.js
@@ -6,11 +6,9 @@ import   './LandingPage.css';
 const LandingPage = ({ children }) =>
 <div>
       <Jumbtron>
-      </Jumbtron>
-      <div className="jumbotron-contents">
         <img src='/assets/images/ph/porthub_logo.png' alt="logo"/>       
          <p> Create a portfolio website and a resume in minutes!<br/>Stay a step ahead of your competitors in the job market and get that job your deserve.</p>
-      </div>      
+      </Jumbtron>   
       <Col size='lg-12' className='d-flex flex-row justify-content-center'>
         <a href='/createsite' className="btn btn-warning landingButton">Create a Portfolio Website</a>
         <a href='/createresume'className="btn btn-info  landingButton">Create a Resume</a>
