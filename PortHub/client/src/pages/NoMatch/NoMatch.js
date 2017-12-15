4510e2cac79f9878c4900dc9dba70bb9eaa90030
Kui <kui_eugenio@hotmail.com>
2017-12-15 11:33:35 -0700

    updated 404 page

--- PortHub/client/src/pages/NoMatch/NoMatch.js
+++ PortHub/client/src/pages/NoMatch/NoMatch.js
@@ -4,10 +4,10 @@ import   './NoMatch.css';
 
 const NoMatch = ({ children }) =>
 <div>
-  <Jumbtron>
+  <div className='jumbotron-contents'>
     <img src='/assets/images/ph/porthub_logo.png' alt="logo" />       
-    <p> 404 Page not found </p>
-  </Jumbtron>
+    <p style={{color:'red', textShadow:'none'}}> oh snap! 404 Page not found </p>
+  </div>
 
 </div>
 export default NoMatch;
