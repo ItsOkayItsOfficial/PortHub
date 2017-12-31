4ed516b1749565099b9949a28e5c05e047118497
Alex P <alexp@itsokayitsofficial.io>
2017-12-31 12:04:45 -0700

    Added console logging for login feedback

--- porthub_native/App.js
+++ porthub_native/App.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './Models';
 
+const onLogin = (email, password) => {
+  console.log(email, password) // user credentials
+}
+
+const onResetPassword = (email) => {
+  console.log(email)
+}
+
 export default class App extends Component {
   render () {
     return (
       <View style={styles.container}>
         <Text>Welcome to the PortHub User App!</Text>
-        <Login />
+        <Login
+        onLogin={onLogin}
+        onResetPassword={onResetPassword}
+        />
       </View>
     );
   }
