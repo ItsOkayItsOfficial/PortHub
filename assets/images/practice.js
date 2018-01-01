5bfefd869e9e2ac70c7a40a375cba411843bfe8e
Jason Summers <jcsummers04@gmail.com>
2018-01-01 12:05:05 -0700

    Pitchdeck added to repo

---
+++ assets/images/practice.js
@@ -0,0 +1,13 @@
+var age = prompt("How old are you?!?");
+
+if(age < 18) {
+    alert("Sorry you are too young!");
+}
+
+else if(age < 21) {
+    alert("You can enter, but you can't drink");
+}
+
+else(age) {
+    console.log("Nice, we're getting weird tonight!");
+}
