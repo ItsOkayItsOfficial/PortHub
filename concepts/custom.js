099b4dabf270e07b046fe8c329206088c6ce0ae0
wcrozier12 <wcc8@nau.edu>
2018-01-02 10:25:39 -0700

    Tightened

--- concepts/custom.js
+++
@@ -1,8 +0,0 @@
-// var frameSrc = "http://itsokayitsofficial.io";
-
-// $('#btnOslo').click(function(){
-//     $('#oslo').on('show', function () {
-//         $('iframe').attr("src",frameSrc);
-// 	});
-//     $('#oslo').modal({show:true})
-// });
