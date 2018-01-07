46b23fcbb45d97c62fe733ffe229793e37a82101
Kui <kui_eugenio@hotmail.com>
2018-01-07 16:48:32 -0700

    created Bootstrap Modal

---
+++ client/src/components/Modal/BootstrapModal.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Modal.css';
+
+
+const BootstrapModal = ({ children, title }) => {
+
+  return (
+  <div className="modal fade" id="viewPDFModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">{title}</h5>
+            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            {children}
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default BootstrapModal;
+
+
