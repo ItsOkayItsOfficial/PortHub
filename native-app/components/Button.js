fb8bc55ae52c0a1ef4d34e42a3cd2ba449628965
Alex P <alexp@itsokayitsofficial.io>
2018-01-08 11:00:31 -0700

    LoginForm updated

--- native-app/components/Button.js
+++ native-app/components/Button.js
@@ -1,38 +1,38 @@
-import { Colors } from '../constants'
-import React, { Component } from 'react'
-import { Button } from 'react-native-elements'
-import { View, StyleSheet } from 'react-native'
+import {Colors} from '../constants'
+import React, {Component} from 'react'
+import {Button} from 'react-native-elements'
+import {View, StyleSheet} from 'react-native'
 
 export default class AppButton extends Component {
 
-render () {
-  return (
+  render() {
+    return (
 
-    <View style={styles.buttonContainer}>
+      <View style={styles.buttonContainer}>
 
-    <Button
-    style={styles.button}
-    backgroundColor={Colors.darkBlue}
-    color={Colors.tintColor}
-    onPress={this.onPress}
-    title={this.title}
-    fontWeight="700"
-    fontFamily="Helvetica"/>
+        <Button
+          style={styles.button}
+          backgroundColor={Colors.darkBlue}
+          color={Colors.tintColor}
+          onPress={this.props.onPress}
+          title={this.props.title}
+          fontWeight="700"
+          fontFamily="Helvetica"/>
 
-   </View>
-      );
-    }
+      </View>
+    );
   }
+}
 
-  const styles = StyleSheet.create({
-    buttonContainer: {
-      flex: 1,
-      justifyContent: 'center',
-      paddingHorizontal: 10
-    },
-    button: {
-      backgroundColor: Colors.darkBlue,
-      alignItems: 'center',
-      padding: 10,
-    }
-  });
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 10
+  },
+  button: {
+    backgroundColor: Colors.darkBlue,
+    alignItems: 'center',
+    padding: 10
+  }
+});
--- native-app/forms/LoginForm.js
+++ native-app/forms/LoginForm.js
@@ -1,63 +1,90 @@
-import { Colors } from '../constants'
-import { Button } from '../components'
-import React, { Component } from 'react'
-import { Ionicons } from '@expo/vector-icons'
-import { View, Text, TextInput, Alert, StyleSheet } from 'react-native'
+import {Colors} from '../constants'
+import {Button} from '../components'
+import React, {Component} from 'react'
+import {Ionicons} from '@expo/vector-icons'
+import axios from 'axios'
+import {View, Text, TextInput, Alert, StyleSheet} from 'react-native'
 
 export default class LoginForm extends Component {
+  state = {
+    error: null,
+    username: '',
+    pin: ''
+  };
 
-  onFocus = () => this.setState({ backgroundColor: 'rgba(42,45,52,1.0)'})
-  onBlur = () => this.setState({ backgroundColor: 'rgba(42,45,52,0.5)' })
+_userLogin = (username) => {
+    axios.get(`http://porthubserver.herokuapp.com/api/users?login=${this.username}`)
+     .then((response) => {
+     return axios.post('/user', response.data)
+     })
+     .then((user) => {
+       console.log("logged in user: ", user.data[0].login);
+       user.data.length===1 ? this.setState({currentUser:user.data[0]}) : this.setState({currentUser:user.data})
+     })
+     .catch(error => {
+       console.log('Error getting user info: ', error.response)
+     })
+   }
 
 
-  render() {
-    return (
-      <View style={styles.container}>
 
-      <View style={styles.inputContainer}>
-        <Ionicons style={styles.inputIcon} name="md-mail" size={25} color={Colors.mainBlue} />
-        <TextInput
-          style={ styles.input }
-          autoCapitalize="none"
-          onSubmitEditing={() => this.passwordInput.focus()}
-          autoCorrect={false}
-          keyboardType="email-address"
-          returnKeyType="next"
-          placeholder="Email"
-          selectionColor= 'white'
-          keyboardAppearance= 'dark'
-          placeholderTextColor={Colors.tintColor}
-        />
-      </View>
+    render() {
+      return (
+        <View style={styles.container}>
 
-      <View style={styles.inputContainer}>
-        <Ionicons style={styles.inputIcon} name="md-lock" size={25} color={Colors.mainBlue} />
-        <TextInput
-          style={styles.input}
-          returnKeyType="go"
-          ref={input => (this.passwordInput = input)}
-          placeholder="Password"
-          placeholderTextColor={Colors.tintColor}
-          selectionColor= 'white'
-          keyboardAppearance= 'dark'
-          secureTextEntry
-        />
-      </View>
+          <View style={styles.inputContainer}>
+            <Ionicons
+              style={styles.inputIcon}
+              name="md-mail"
+              size={20}
+              color={Colors.mainBlue}/>
 
-          <Button
-            onPress={ this.props.onLoginPress }
-            title="LOGIN"
-          />
+            <TextInput
+              style={styles.input}
+              autoCapitalize="none"
+              onSubmitEditing={() => this.passwordInput.focus()}
+              refs={input => (this.state.password= input)}
+              autoCorrect={false}
+              keyboardType="email-address"
+              returnKeyType="next"
+              placeholder="Username"
+              selectionColor='white'
+              keyboardAppearance='dark'
+              placeholderTextColor={Colors.tintColor}
+              onChangeText={input => this.setState({username})}
+              />
+          </View>
 
-      </View>
-    );
-  }
+          <View style={styles.inputContainer}>
+            <Ionicons
+              style={styles.inputIcon}
+              name="md-lock"
+              size={20}
+              color={Colors.mainBlue}/>
+
+            <TextInput
+              style={styles.input}
+              returnKeyType="go"
+              ref={input => (this.passwordInput = input)}
+              placeholder="PIN"
+              placeholderTextColor={Colors.tintColor}
+              selectionColor='white'
+              keyboardAppearance='dark'
+              secureTextEntry
+              />
+          </View>
+
+          <Button onPress={this._userLogin} title="LOGIN"/>
+
+        </View>
+      );
+    }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 50,
+    padding: 50
   },
   inputContainer: {
     marginBottom: 30,
@@ -65,17 +92,17 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
     borderBottomWidth: 3,
     borderBottomColor: Colors.tintColor,
-    alignItems: 'baseline',
+    alignItems: 'baseline'
   },
   inputIcon: {
     paddingRight: 10,
-    bottom: 0,
-},
+    bottom: 0
+  },
   input: {
     fontSize: 20,
     flex: 1,
     color: '#fff',
     fontWeight: '700',
-    bottom: 0,
-  },
+    bottom: 0
+  }
 });
