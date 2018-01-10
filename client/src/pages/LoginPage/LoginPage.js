f01c8f280b560b38b7f0a7bcbe775f670e8cb80d
wcrozier12 <wcc8@nau.edu>
2018-01-10 13:46:54 -0700

    added login tooltip

--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Jumbotron from '../../components/Jumbotron';
 import  './LoginPage.css';
+import ReactTooltip from 'react-tooltip';
 
 const LoginPage = ({ children, ghRedirect, localLogin }) =>
 <div>
@@ -14,19 +15,20 @@ const LoginPage = ({ children, ghRedirect, localLogin }) =>
 
         <div className="col-lg-6 border border-dark border-left-0 border-bottom-0 border-top-0" style={{boxShadow:'none'}}>
             <form>
-              <div className="form-group">
+              <div className="form-group" data-tip='React-tooltip'>
                 <label htmlFor="exampleInputEmail">Email address</label>
-                <input type="email" disabled title="disabled for this version" className="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter email" />
+                <input type="email" disabled data-tip='React-tooltip' className="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter email" />
                 <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
               </div>
-              <div className="form-group">
+              <div className="form-group" data-tip='React-tooltip'>
                 <label htmlFor="exampleInputPassword">Password</label>
-                <input type="password" disabled title="disabled for this version" className="form-control" id="exampleInputPassword" placeholder="Password" />
+                <input type="password" disabled data-tip='React-tooltip' className="form-control" id="exampleInputPassword" placeholder="Password" />
               </div>
               <div className="text-center">
                 <button type="submit" title="disabled for this version" disabled onClick={localLogin} className="btn btn-primary">Submit</button>
               </div>
             </form>
+            <ReactTooltip> Disabled for this version. </ReactTooltip>
         </div>
 
     <div className="col-lg-6 mx-auto text-center">
