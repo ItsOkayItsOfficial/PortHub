82bb9010b83970c2784d53ee3c007f127237072e
Kui <kui_eugenio@hotmail.com>
2018-01-12 10:34:44 -0700

    cleaned up CreateSitesuccess and CreateSiteError formatting

--- client/src/pages/LoadingPages/CreateSiteLoader/CreateSiteMessage/CreateSiteError.js
+++ client/src/pages/LoadingPages/CreateSiteLoader/CreateSiteMessage/CreateSiteError.js
@@ -6,12 +6,20 @@ const CreateSiteError = ({ login, file }) => {
     <div className='d-flex flex-column p-2 justify-content-center align-items-center successContent'>
         <h1 className='p-2 successHeader'>Sorry about that..</h1>
         <iframe title='404' src="https://giphy.com/embed/MQ4gUFfumJppm" width="240" height="360" frameBorder="0" className="giphy-embed m-0" allowFullScreen></iframe><p></p>
-        <h4>An error occured while trying to make your site! Our bad. Please choose an option below to restart</h4>
-        <div className='flex-row'>
-          <a href='/createsite' className="btn btn-warning successButton">Create a Portfolio Website</a>
-          <a href='/createresume' className="btn btn-info successButton">Create a Resume</a>
+        <h4>An error occured while trying to make your site! Our bad. Please choose an option to restart</h4>
+
+        <div className="row w-100 w-100 d-flex flex-row justify-content-center">
+                    {/* Buttons */}
+                    <div id='buttons' className="row mx-auto d-flex flex-row justify-content-around" style={{top:"auto"}}>
+                          <div className="col-lg-4 col-sm-8 d-flex text-center">
+                            <a href='/createsite' className="btn btn-warning mx-4 w-100 d-md-block btn-landingPage">Create a Portfolio Website</a>
+                          </div>
+                          <div className="col-lg-4 col-sm-8 d-flex text-center">
+                              <a href='/createresume' className="btn btn-info mx-4 w-100 d-md-block btn-landingPage">Create a Resume</a>
+                          </div>
+                    </div>              
+                </div>
         </div>
-    </div>
   )
 }
 
--- client/src/pages/LoadingPages/CreateSiteLoader/CreateSiteMessage/CreateSiteSuccess.js
+++ client/src/pages/LoadingPages/CreateSiteLoader/CreateSiteMessage/CreateSiteSuccess.js
@@ -3,48 +3,78 @@ import React from 'react';
 
 const CreateSiteMessage = ({ login, file }) => {
   return (
-    <div className='CreateSiteSuccessContainer' style={{height:"100vh"}}>
-          <div className="row">
-              <div className="row w-100 d-flex justify-content-center">
-                  <span className="title">Congratulations!</span>
-              </div>
-              <div className="row w-100 d-flex flex-row justify-content-around">
-                    <div className="col-lg-6 d-flex flex-column justify-content-around align-items-stretch">
-                        <div>
-                          <p><i className="fa fa-globe fa-3x"></i></p>
-                        </div>
-                        <div>
-                          <p className="text-align-center">View Your Site!</p>
-                          <p className="text-align-center">Your new personal portfolio site is now hosted at</p>
-                          <p><a href={`https://${login}.github.io/${file.split('.').shift()}`} target='_blank' rel="noopener noreferrer">{`${login}.github.io/${file.split('.').shift()}`}</a>. </p>
-                          <p className="text-align-center">However, it may take a few minutes to appear.</p>
+    <div>
+        <div class="d-none d-lg-block">
+            <div className='CreateSiteSuccessContainer' style={{height:"100vh"}}>
+                <div className="row d-flex flex-row align-items-center" style={{width:"100vw", height:"15vh"}}>
+                            <span className="title w-100 text-center">Congratulations!</span>
+                </div>
+                <div className="row w-100 d-flex flex-row align-items-center" style={{width:"100vw", height:"33vh"}}>
+                    <div className="col-lg-5 d-flex flex-column align-items-center justify-content-center">
+                        <div className="col-10 offset-1 d-flex flex-column justify-content-center align-items-center">
+                            <div>
+                                <p><i className="fa fa-globe fa-3x"></i></p>
+                            </div>
+                            <div className="mt-4">
+                                <p className="text-center">View Your Site!</p>
+                                <p className="text-center">Your new personal portfolio site is now hosted at</p>
+                                <p><a href={`https://${login}.github.io/${file.split('.').shift()}`} target='_blank' rel="noopener noreferrer">{`${login}.github.io/${file.split('.').shift()}`}</a>. </p>
+                                <p className="text-center">However, it may take a few minutes to appear.</p>
+                            </div>
                         </div>
                     </div>
                     
-                    <div className="col-lg-6 d-flex flex-column justify-content-start align-items-center p-0">
-                          <p>
-                              Here are a few steps you may want to take to in the meantime:
-                          </p>
-                          <p>
-                              Edit the name of your portfolio file <a href={`https://github.com/${login}/${login}.github.io/edit/master/${file}`} target='_blank'>here</a>. Our names are chosen at random. <strong>NOTE:</strong> This will change the name of the site your portfolio is hosted at, it will now be {`www.${login}.github.io/<newfilename>`}                    
-                          </p>
-                          <p>
-                              Clone your GitHub pages repository to your local computer. This will allow you to play around and customize even more, and then add your changes directly to your hosted portfolio site using git. For more info on cloning a repository go  <a href='https://help.github.com/articles/cloning-a-repository/' target='_blank' rel="noopener noreferrer">here</a>.
-                          </p>
+                    <div className="col-lg-7 d-flex flex-column justify-content-center align-items-center">
+                        <div className="col-10 offset-1 d-flex flex-column justify-content-center align-items-center">
+                            <p>Here are a few steps you may want to take to in the meantime:</p>
+                            <ul>
+                                <li> 
+                                    Edit the name of your portfolio file <a href={`https://github.com/${login}/${login}.github.io/edit/master/${file}`} target='_blank'>here</a>. Our names are chosen at random. <strong>NOTE:</strong> This will change the name of the site your portfolio is hosted at, it will now be {`www.${login}.github.io/<newfilename>`}
+                                </li>                    
+                                <li>
+                                    Clone your GitHub pages repository to your local computer. This will allow you to play around and customize even more, and then add your changes directly to your hosted portfolio site using git. For more info on cloning a repository go  <a href='https://help.github.com/articles/cloning-a-repository/' target='_blank' rel="noopener noreferrer">here</a>.
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-              </div>
-              <div className="row w-100 d-flex flex-row justify-content-around">
-                    {/* Buttons */}
-                    <div id='buttons' className="row mx-auto d-flex flex-row justify-content-around">
-                          <div className="col-lg-4 col-sm-8 d-flex text-center">
-                            <a href='/createsite' className="btn btn-warning mx-4 w-100 d-md-block btn-landingPage">Create a Portfolio Website</a>
-                          </div>
-                          <div className="col-lg-4 col-sm-8 d-flex text-center">
-                              <a href='/createresume' className="btn btn-info mx-4 w-100 d-md-block btn-landingPage">Create a Resume</a>
-                          </div>
-                    </div>              
-              </div>
-          </div>
+                </div>
+                <div className="row w-100 d-flex flex-row justify-content-around" id='buttons'>
+                    <div className="col-lg-4 col-sm-8 d-flex text-center">
+                    <a href='/createsite' className="btn btn-warning mx-4 w-100 d-md-block btn-landingPage">Create a Portfolio Website</a>
+                    </div>
+                    <div className="col-lg-4 col-sm-8 d-flex text-center">
+                        <a href='/createresume' className="btn btn-info mx-4 w-100 d-md-block btn-landingPage">Create a Resume</a>
+                    </div>
+                </div>              
+            </div>
+        </div>
+        {/* ------ small device ------- */}
+        <div class="d-md-block" style={{height:"100vh"}}>
+            <div className="row d-flex flex-row align-items-center" style={{width:"100vw", height:"15vh"}}>
+                <span className="title w-100 text-center display-4">Congratulations!</span>
+            </div>        
+            <div className="row">
+                <div className="col-10 offset-1 d-flex flex-column justify-content-center align-items-center">
+                    <div>
+                        <p><i className="fa fa-globe fa-3x"></i></p>
+                    </div>
+                    <div className="mt-4">
+                        <p className="text-center">View Your Site!</p>
+                        <p className="text-center">Your new personal portfolio site is now hosted at</p>
+                        <p><a href={`https://${login}.github.io/${file.split('.').shift()}`} target='_blank' rel="noopener noreferrer">{`${login}.github.io/${file.split('.').shift()}`}</a>. </p>
+                        <p className="text-center">However, it may take a few minutes to appear.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="row w-100 mt-4 d-flex flex-row">
+                    <div className="col-sm-12">
+                    <a href='/createsite' className="btn btn-warning mx-4 w-100 d-md-block btn-landingPage">Create a Portfolio Website</a>
+                    </div>
+                    <div className="col-sm-12">
+                        <a href='/createresume' className="btn btn-info mx-4 w-100 d-md-block btn-landingPage">Create a Resume</a>
+                    </div>
+            </div>            
+        </div>
     </div>
   )
 }
